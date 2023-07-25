5bd3008d729c19d7c2a66e2d7f1475f5685f1c8e
nabil <nabil.nadzor@gmail.com>
2023-07-25 18:45:27 +0200

    Lesson 39

--- src/main/lesson_39_Iterator/homework/fibonacci/FibonacciAppl.java
+++ src/main/lesson_39_Iterator/homework/fibonacci/FibonacciAppl.java
@@ -5,7 +5,6 @@ import main.lesson_39_Iterator.homework.fibonacci.model.Fibonacci;
 public class FibonacciAppl {
     public static void main(String[] args) {
         Fibonacci fibonacci = new Fibonacci(8);
-        //TODO print all numbers, and reduce sum
         //1,1,2,3,5,8,13,21
         //sum = 54
         int sum = 0;
